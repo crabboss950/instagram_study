de0e24261f1bce059ed1e0ce14bffccfb647ebd6
crabboss950 <silverbell950@naver.com>
2022-12-09 08:29:13 +0900

    python turtle 시작

---
+++ python_turtle/turtle.py
@@ -0,0 +1,14 @@
+# 사용법
+# https://docs.python.org/ko/3/library/turtle.html
+# https://blog.naver.com/python_math/221214856867
+
+import turtle
+
+turtle.setup(width=600, height=600)
+t=turtle.Turtle()
+t.shape("turtle")
+
+t.home()
+
+input("Press asny key to exit!")
+
