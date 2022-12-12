471059814e9d3fd025bfaba7b2ae70b9bf1439bc
crabboss950 <silverbell950@naver.com>
2022-12-12 18:52:07 +0900

    .

--- popup/pop_up.js
+++ popup/pop_up.js
@@ -10,6 +10,9 @@ for(var i = 0; i < target.length; i++){
   target[i].addEventListener('click', function(){
     targetID = this.getAttribute('href');
     document.querySelector(targetID).style.display = 'block';
+    // 팝업을 열고 로딩 기간 함수 삽입 
+    // 이미지를 랜덤으로 뽑는 함수를 삽입 (로또번호 뽑아서 index.html에 꽂아넣는것처럼 하면 되지 않을까?)
+    // document.getElementById('pop').innerHTML = document.getElementById('pop').innerHTML + '\n' + Select_Number;
   });
 }
 
